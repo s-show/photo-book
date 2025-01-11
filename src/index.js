50297a5cb63977bf5c4a923196e913ff5e60dee6
s-show <s1shimz@gmail.com>
2025-01-12 00:07:33 +0900

    画像縮小機能を追加

--- src/index.js
+++ src/index.js
@@ -1,84 +1,37 @@
 document.getElementById("addFileBtn").addEventListener("click", (e) => {
   document.getElementById("fileSelector").click();
 });
-document.getElementById("fileSelector").addEventListener("change", (ev) => {
-  if (ev.target.files.length == 0) {
+document.getElementById("fileSelector").addEventListener("change", (e) => {
+  if (e.target.files.length == 0) {
+    console.log("file not selected.");
     return;
   }
-  let readErrorCount = 0;
-  let readSuccessCount = 0;
-  let readNotImageCount = 0;
-  let readFileCount = 0;
-  let imageFiles = [];
-  const imageCountSpan = document.getElementById("imageCount");
-  const readSuccessCountSpan = document.getElementById("readSuccessCount");
-  const readErrorCountSpan = document.getElementById("readErrorCount");
-  const readNotImageCountSpan = document.getElementById("readNotImageCount");
-  // 読み込むファイルの拡張子を指定
-  const fileTypes = ["jpg", "jpeg", "png", "bmp", "gif", "heic"];
-
-  imageCountSpan.innerText = 0;
-  readSuccessCountSpan.innerText = 0;
-  readErrorCountSpan.innerText = 0;
-  readNotImageCountSpan.innerText = 0;
+  const imageFiles = [];
+  let fileCount = 0;
 
   toggleLoadingAnimation(true);
-  imageCountSpan.innerText = ev.target.files.length;
 
-  Array.from(ev.target.files).forEach((file) => {
-    // まず拡張子で画像ファイルか判断し、読み込み後にマジックナンバーを確認して
-    // img.src に読み込ませるか判断する。
-    let extension = file.name.split(".").pop().toLowerCase();
-    let isImageExtension = fileTypes.indexOf(extension) > -1;
-    if (!isImageExtension) {
-      ++readNotImageCount;
-      console.log("not image file: " + readNotImageCount);
-      readNotImageCountSpan.innerText = readNotImageCount;
-      return;
-    }
-
-    let fileReader = new FileReader();
-    fileReader.readAsArrayBuffer(file);
-    fileReader.addEventListener(
-      "loadstart",
-      (() => {
-        console.log("loading: " + file.name);
-      })(file),
-    );
+  Array.from(e.target.files).forEach((file, index, array) => {
+    const fileReader = new FileReader();
     fileReader.addEventListener("error", () => {
-      ++readErrorCount;
-      console.error("read error: " + readErrorCount);
-      readFileCount = readSuccessCount + readErrorCount;
-      readErrorCountSpan.innerText = readErrorCount;
-      if (readFileCount == ev.target.files.length) {
+      console.error("read error: " + file.name);
+      fileCount++;
+      if (fileCount == array.length) {
         imageListDom(imageFiles);
       }
     });
-    fileReader.addEventListener(
-      "load",
-      (() => {
-        return (e) => {
-          if (!getImageFormat(e.target.result)) {
-            ++readNotImageCount;
-            console.warn(file.name + " is not image file.");
-            console.warn("not image file: " + readNotImageCount);
-            readNotImageCountSpan.innerText = readNotImageCount;
-          } else {
-            let imageFile = { name: "", image: "" };
-            ++readSuccessCount;
-            readSuccessCountSpan.innerText = readSuccessCount;
-            imageFile.name = file.name;
-            imageFile.image = e.target.result;
-            imageFiles.push(imageFile);
-          }
-          readFileCount = readSuccessCount + readErrorCount + readNotImageCount;
-          if (readFileCount == ev.target.files.length) {
-            imageListDom(imageFiles);
-            toggleLoadingAnimation(false);
-          }
-        };
-      })(file),
-    );
+    fileReader.addEventListener("load", (e) => {
+      fileCount++;
+      const imageFile = { name: "", image: "" };
+      imageFile.name = file.name;
+      imageFile.image = e.target.result;
+      imageFiles.push(imageFile);
+      if (fileCount == array.length) {
+        imageListDom(imageFiles);
+        toggleLoadingAnimation(false);
+      }
+    });
+    fileReader.readAsDataURL(file);
   });
 });
 
@@ -91,10 +44,10 @@ elm.addEventListener("dragstart", (event) => {
   }
 });
 elm.addEventListener("dragover", (event) => {
-  let underImage = document.getElementById(event.target.id);
+  const underImage = document.getElementById(event.target.id);
   if (event.target.classList.contains("imageDiv")) {
     event.preventDefault();
-    let rect = underImage.getBoundingClientRect();
+    const rect = underImage.getBoundingClientRect();
     if ((event.clientY - rect.top) < (underImage.clientHeight / 2)) {
       // console.info(rect.top);
       //マウスカーソルの位置が要素の半分より上
@@ -108,20 +61,20 @@ elm.addEventListener("dragover", (event) => {
   }
 });
 elm.addEventListener("dragleave", (event) => {
-  let underImage = document.getElementById(event.target.id);
+  const underImage = document.getElementById(event.target.id);
   if (event.target.classList.contains("imageDiv")) {
     underImage.style.borderTop = "";
     underImage.style.borderBottom = "";
   }
 });
 elm.addEventListener("drop", (event) => {
-  let underImage = document.getElementById(event.target.id);
+  const underImage = document.getElementById(event.target.id);
   if (event.target.classList.contains("imageDiv")) {
     event.preventDefault();
-    let id = event.dataTransfer.getData("text/plain");
-    let elm_drag = document.getElementById(id);
+    const id = event.dataTransfer.getData("text/plain");
+    const elm_drag = document.getElementById(id);
 
-    let rect = underImage.getBoundingClientRect();
+    const rect = underImage.getBoundingClientRect();
     if ((event.clientY - rect.top) < (underImage.clientHeight / 2)) {
       //マウスカーソルの位置が要素の半分より上
       underImage.parentNode.insertBefore(elm_drag, underImage);
@@ -133,23 +86,16 @@ elm.addEventListener("drop", (event) => {
     underImage.style.borderBottom = "";
   }
 });
- 
+
 // 写真横のファイル名を変更する処理と写真の個別削除
 elm.addEventListener("dblclick", (e) => {
   if (e.target.classList.contains("imageCaption")) {
-    let result = prompt("変更後のファイル名を入力してください");
+    const result = prompt("変更後のファイル名を入力してください");
     if (result != null) {
       document.getElementById(e.target.id).innerText = result;
     }
   } else if (e.target.classList.contains("thumb")) {
     elm.removeChild(e.target.parentNode);
-    let imageCount = document.getElementById("imageCount").innerText;
-    let readSuccessCount =
-      document.getElementById("readSuccessCount").innerText;
-    --imageCount;
-    --readSuccessCount;
-    document.getElementById("imageCount").innerText = imageCount;
-    document.getElementById("readSuccessCount").innerText = readSuccessCount;
   }
 });
 
@@ -160,32 +106,28 @@ document.getElementById("headerText").addEventListener("input", (e) => {
 
 // 画像の幅を動的に変更する設定（表示と印刷両方を同時に変更）
 document.getElementById("imageWidth").addEventListener("input", (e) => {
-  const thumbClass = document.querySelectorAll('.thumb');
+  const thumbClass = document.querySelectorAll(".thumb");
   thumbClass.forEach((elem) => {
-    elem.style.setProperty('width', e.target.value + 'px');
+    elem.style.setProperty("width", e.target.value + "px");
     console.log(elem.clientWidth);
     console.log(elem.clientHeight);
-  })
+  });
 });
 
 // 画像の全削除処理
 document.getElementById("deleteFileBtn").addEventListener("click", () => {
-  let divImageList = document.getElementById("imageList");
+  const divImageList = document.getElementById("imageList");
   while (divImageList.firstChild) {
     divImageList.removeChild(divImageList.firstChild);
   }
-  document.getElementById("imageCount").innerText = 0;
-  document.getElementById("readSuccessCount").innerText = 0;
-  document.getElementById("readErrorCount").innerText = 0;
-  document.getElementById("readNotImageCount").innerText = 0;
   document.getElementById("fileSelector").value = "";
 });
 
 // ファイル読み込み中のローディング表示切り替え関数
 function toggleLoadingAnimation(signal) {
-  let loadingMessageDiv = document.getElementById("loadingMessage");
-  let loadingWrapperDiv = document.getElementById("content");
-  let loadingDiv = document.getElementById("loading");
+  const loadingMessageDiv = document.getElementById("loadingMessage");
+  const loadingWrapperDiv = document.getElementById("content");
+  const loadingDiv = document.getElementById("loading");
   if (signal) {
     loadingWrapperDiv.classList.add("loadingWrapper");
     loadingDiv.classList.add("loading");
@@ -194,33 +136,6 @@ function toggleLoadingAnimation(signal) {
     loadingWrapperDiv.classList.remove("loadingWrapper");
     loadingDiv.classList.remove("loading");
     loadingMessageDiv.style.display = "none";
-  }
-}
-
-// 読み込んだファイルが画像ファイルならファイルタイプを返し、
-// 画像ファイル以外なら false を返す関数
-// https://qiita.com/hal9188/items/a1378f5b101e991609a8
-function getImageFormat(arrayBuffer) {
-  let arr = new Uint8Array(arrayBuffer).subarray(0, 6);
-  let header = "";
-
-  for (let i = 0; i < arr.length; i++) {
-    header += arr[i].toString(16);
-  }
-
-  switch (true) {
-    case /^89504e47d/.test(header):
-      return "image/png";
-    case /^ffd8ff/.test(header):
-      return "image/jpeg";
-    case /^000206674/.test(header):
-      return "image/heic";
-    case /^4749463839/.test(header):
-      return "image/gif";
-    case /^424d/.test(header):
-      return "image/bmp";
-    default:
-      return false;
   }
 }
 
@@ -238,16 +153,16 @@ function imageListDom(imageFiles) {
     return 0;
   });
   imageFiles.forEach((imageFile) => {
-    let imageDiv = document.createElement("div");
+    const imageDiv = document.createElement("div");
     imageDiv.className = "imageDiv";
     imageDiv.setAttribute("id", imageFile.name + "_Div");
     imageDiv.setAttribute("draggable", true);
 
-    let image = document.createElement("img");
+    const image = document.createElement("img");
     image.className = "thumb";
-    image.src = arrayBufferToDataURL(imageFile.image);
+    image.src = imageFile.image;
 
-    let imageCaption = document.createElement("span");
+    const imageCaption = document.createElement("span");
     imageCaption.innerText = imageFile.name.split(".")[0];
     imageCaption.className = "imageCaption";
     imageCaption.setAttribute("id", imageFile.name + "_Span");
@@ -258,17 +173,20 @@ function imageListDom(imageFiles) {
   });
 }
 
-// arrayBuffer 型の画像データを DataURL に変換する
-// (getImageFormat 関数のために arrayBuffer 型で読み込んでいる)
-function arrayBufferToDataURL(arrayBuffer) {
-  let binary = "";
-  let bytes = new Uint8Array(arrayBuffer);
-  bytes.forEach((byte) => {
-    binary += String.fromCharCode(byte);
-  });
-  return `data:${getImageFormat(arrayBuffer)};base64,${window.btoa(binary)}`;
+function resizeImage(image) {
+    const canvas = document.createElement("canvas");
+  // 表示サイズで縮小すると画像が荒くなりすぎるので、表示サイズの2倍に縮小している。
+    canvas.width = image.clientWidth * 2.0;
+    canvas.height = image.clientHeight * 2.0;
+    const context = canvas.getContext("2d");
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL("image/png");
 }
 
-// function resizeImage(image) {
-//
-// }
+document.getElementById('reduceImage').addEventListener('click', () => {
+  const imageElements = document.querySelectorAll(".thumb");
+  imageElements.forEach((imageElement) => {
+    imageElement.src = resizeImage(imageElement);
+  });
+  globalThis.alert('画像を縮小しました')
+})
